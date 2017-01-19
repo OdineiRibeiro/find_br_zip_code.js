35d2ee61b3fa04304cb77a33315f5479abf29ff5
Odinei Rocha <odinei.ribeiro@live.com>
2017-01-19 21:53:17 -0200

    Find your zip code! :mailbox:

---
+++ README.md
@@ -0,0 +1,7 @@
+# find_br_zip_code.js
+
+Find your zip code with AJAX!
+
+But it works only in Brazil :(
+
+You can see the page here and check the original source on [Alura's blog](http://blog.alura.com.br/preenchendo-formulario-html-automaticamente-com-ajax/)
---
+++ find_br_zip_code.js
@@ -0,0 +1,24 @@
+function preencheCampos(json) {
+  document.querySelector('input[name=endereco]').value = json.logradouro;
+  document.querySelector('input[name=bairro]').value = json.bairro;
+  document.querySelector('input[name=complemento]').value = json.complemento;
+  document.querySelector('input[name=cidade]').value = json.localidade;
+  document.querySelector('input[name=estado]').value = json.uf;
+}
+
+function buscaCep() {
+  var inputCep = document.querySelector('input[name=cep]');
+  var cep = inputCep.value.replace('-', '');
+  var url = 'http://viacep.com.br/ws/' + cep + '/json';
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status = 200) {
+        preencheCampos(JSON.parse(xhr.responseText));
+      }
+    }
+  }
+  xhr.send();
+}
